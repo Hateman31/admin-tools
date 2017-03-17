718ebe941e75437907382432ee77ba1d88165ac0
metilogrunge31@gmail.com <metilogrunge31@gmail.com>
2017-03-17 17:31:07 +0300

    reading list

--- cleaner/cleaner.php
+++ cleaner/cleaner.php
@@ -3,7 +3,22 @@
 include('../Classes/PHPexcel.php');
 
 if ( isset($_FILES['uploadfile']) ){
-	echo $_FILES['uploadfile']['name'] . " was uploaded!";
+	echo $_FILES['uploadfile']['name'] . " was uploaded!","<br>";
+	
+	$xls = $_FILES['uploadfile']['tmp_name'];
+	$obj = PHPExcel_IOFactory::load($xls);
+	$ws = $obj->setActiveSheetIndex(1);
+	$table = $obj->getActiveSheet()->toArray();
+	
+	//~ var_dump($table);
+	
+	foreach($table as $row) {
+		echo '<tr>';
+		// Цикл по колонкам
+		foreach( $row as $col ) {
+			echo '<td>'.$col.'</td>';
+		}	
+	}
 } else {
 	echo "Error!";
 }
