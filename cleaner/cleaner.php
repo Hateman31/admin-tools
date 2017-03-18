cd961db9fdf3196a7695f11d8390bda373e3dcf9
Hateman31 <metilogrunge31@gmail.com>
2017-03-18 18:57:40 +0300

    read coulmn comment

--- cleaner/cleaner.php
+++ cleaner/cleaner.php
@@ -3,21 +3,27 @@
 include('../Classes/PHPexcel.php');
 
 if ( isset($_FILES['uploadfile']) ){
-	echo $_FILES['uploadfile']['name'] . " was uploaded!","<br>";
 	
 	$xls = $_FILES['uploadfile']['tmp_name'];
 	$obj = PHPExcel_IOFactory::load($xls);
 	$obj->setActiveSheetIndex(0);
 	$table = $obj->getActiveSheet()->toArray();
 	
-	foreach($table as $row) {
-		echo '<tr>';
-		// Цикл по колонкам
-		foreach( $row as $col ) {
-			echo $col.' ';
-		}	
-		echo '<br>';
+	//~ 1) столбца 'COMMENT' нет
+	//~ 2) в перой строке нет имен
+	//~ ***
+	$column_num = -1;
+	foreach( $table[0] as $key=>$name ) {
+		if (mb_strtoupper($name) == 'COMMENT') {
+			$column_id = $key;
+			break;
+		}
+	}	
+	
+	foreach( array_slice($table,1) as $row ) {
+		echo $row[column_id];
 	}
+	
 } else {
-	echo "Error!";
+	die("Error!");
 }
